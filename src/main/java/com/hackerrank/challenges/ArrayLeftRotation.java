3a26a96d7c0601aaa6f75fef237888efbe8e812a
eric-anunciacao <anunciacao.eric@gmail.com>
2019-11-22 11:40:16 -0300

    Add Array Left Rotation Challenge

---
+++ src/main/java/com/hackerrank/challenges/ArrayLeftRotation.java
@@ -0,0 +1,15 @@
+package com.hackerrank.challenges;
+
+import java.util.List;
+
+import com.hackerrank.util.FileUtils;
+
+public class ArrayLeftRotation {
+
+	public static void main(String[] args) {
+		List<String> lines = FileUtils.getFileLines("array-left-rotation.txt");
+		if (!lines.isEmpty()) {
+			
+		}
+	}
+}
