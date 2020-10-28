92f42b615499017974e9e123bbbf6d44fab33771
Eric Anunciação <ericanunciacao@MacBook-Pro-de-Eric.local>
2020-10-27 22:44:20 -0300

    Removing lombok dependency

--- src/main/java/com/hackerrank/model/Stock.java
+++ src/main/java/com/hackerrank/model/Stock.java
@@ -2,20 +2,34 @@ package com.hackerrank.model;
 
 import java.util.List;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
-
-@Getter
-@Setter
-@ToString
-@NoArgsConstructor
 public class Stock {
 
 	private int numberOfPrices;
-
 	private List<Integer> prices;
-
 	private long profit;
+
+	public int getNumberOfPrices() {
+		return numberOfPrices;
+	}
+
+	public void setNumberOfPrices(int numberOfPrices) {
+		this.numberOfPrices = numberOfPrices;
+	}
+
+	public List<Integer> getPrices() {
+		return prices;
+	}
+
+	public void setPrices(List<Integer> prices) {
+		this.prices = prices;
+	}
+
+	public long getProfit() {
+		return profit;
+	}
+
+	public void setProfit(long profit) {
+		this.profit = profit;
+	}
+
 }
